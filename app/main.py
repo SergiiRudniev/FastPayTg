2afda6a2b9924d398f5bddc6b2956a0d676696cd
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-06-09 21:25:56 +0200

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    ...
+import grpc
+import db_pb2, db_pb2_grpc
+import cProfile
+
+
+def run_client():
+    channel = grpc.insecure_channel('localhost:5040')
+    stub = db_pb2_grpc.dbStub(channel)
+
+    request = db_pb2.DBRequestCreateNewUser(Id="id")
+    response = stub.CreateNewUser(request)
+    print("Результат:", response.Status)
+
+    request = db_pb2.DBRequestGetBalance(Id="id")
+    response = stub.CreateNewUser(request)
+
+    print("Результат:", response.Status)
+
+if __name__ == '__main__':
+    cProfile.run('run_client()')
