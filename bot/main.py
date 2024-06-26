186b6151966636f4845867fba10d5202e5e08014
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-06-26 22:11:01 +0200

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from dotenv import load_dotenv
 from os import getenv
 import sys
@@ -7,9 +9,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
-import keyboard
 
-keyboard.Start()
 load_dotenv()
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
@@ -18,9 +18,11 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Для запуска нажми \"Start\"", reply_markup=keyboard.StartKeyBoards)
+    await message.answer(f"Для запуска нажми \"FastPay\"")
 
 
 async def main() -> None:
     bot = Bot(token=getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
+
+asyncio.run(main())
