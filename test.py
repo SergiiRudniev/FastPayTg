4c2e783126788531a2ba88648dfbaf667d20c069
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-07-23 23:34:30 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import requests
 key = 631097127
 value = 10.28
 
-url = f"http://localhost:9090/set/{key}"
-payload = {"value": value}
+url = f"http://localhost:8080/api/send/6033910454"
+payload = {"PayerId": "881637263", "Amount": 100}
 headers = {"Content-Type": "application/json"}
 response = requests.post(url, json=payload, headers=headers)
+print(response)
