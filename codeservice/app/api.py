d689cd2a97d0c8e61548b2429dd29afb7b8bcd16
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-07-29 22:49:30 +0200

    Add files via upload

---
+++ codeservice/app/api.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI, HTTPException
+import redis
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+redis_client = redis.StrictRedis(host='redis-service', port=6379, db=0)
+
+
+class KeyValue(BaseModel):
+    value: str
+
+@app.post("/set/{key}")
+def create_code(key: str, key_value: KeyValue):
+    value = key_value.value
+    redis_client.set(key, value, ex=60)
+    return {"message": f"Key '{key}' set to '{value}'"}
+
+
+@app.get("/get/{key}")
+def get_value(key: str):
+    value = redis_client.get(key)
+    if value:
+        return {"key": key, "value": value.decode('utf-8')}
+
+    result = collection.find_one({"key": key})
+    if result:
+        redis_client.set(key, result["value"], ex=60)
+        return {"key": key, "value": result["value"]}
+
+    raise HTTPException(status_code=404, detail="Key not found")
+
+
+@app.delete("/delete/{key}")
+def delete_value(key: str):
+    result = collection.delete_one({"key": key})
+    if result.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="Key not found")
+
+    redis_client.delete(key)
+    return {"message": f"Key '{key}' deleted"}
