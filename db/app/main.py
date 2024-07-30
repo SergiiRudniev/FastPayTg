bb36a58434a989ca4170f30e3f677034d70bb06b
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-07-30 23:36:00 +0200

    Add files via upload

--- db/app/main.py
+++ db/app/main.py
@@ -14,12 +14,10 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-mongo_client = MongoClient("mongodb://mongo:27017")
+mongo_client = MongoClient("mongodb://mongo-service:27017")
 db = mongo_client["key_value_db"]
 collection = db["key_value_collection"]
-
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
-
 
 class KeyValue(BaseModel):
     value: str
