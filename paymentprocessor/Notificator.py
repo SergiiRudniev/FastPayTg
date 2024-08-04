52cfb1109e052c30384a2844189dc8a1970d5638
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-08-04 23:40:59 +0200

    Add files via upload

---
+++ paymentprocessor/Notificator.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class Notificator:
+    def __init__(self):
+        self.url = "notificator:345"
+
+    def SendSuccessfullyMoneyTransfer(self, chat_id, amount, recipient_id):
+        data = {
+            "chat_id": chat_id,
+            "amount": amount,
+            "recipient_id": recipient_id,
+        }
+        requests.post(f"http://{self.url}/successfully_money_transfer", json=data)
+
+    def SendUnsuccessfullyMoneyTransfer(self, chat_id, amount, recipient_id):
+        data = {
+            "chat_id": chat_id,
+            "amount": amount,
+            "recipient_id": recipient_id
+        }
+        requests.post(f"http://{self.url}/unsuccessfully_money_transfer", json=data)
+
--- paymentprocessor/tasks.py
+++ paymentprocessor/tasks.py
@@ -1,15 +1,23 @@
 from celery_app import app
 from PaySystem import PaySystem
 import logging
+from Notificator import Notificator
 
 pay_system = PaySystem()
-
+notificator = Notificator()
 
 @app.task
 def add_to_queue(data):
     print(f"Worker processing data: {data}")
     try:
         logging.info(f"Processing payment: recipient_id={data.get("recipient_id")}, amount={data.get("Amount")}, payer_id={data.get("Payerid")}")
-        pay_system.Send(str(data.get("recipient_id")), str(data.get("Payerid")), int(data.get("Amount")))
+        response = pay_system.Send(str(data.get("recipient_id")), str(data.get("Payerid")), int(data.get("Amount")))
+        if response != None:
+            notificator.SendSuccessfullyMoneyTransfer(int(data.get("Payerid")), int(data.get("Amount")), int(data.get("recipient_id")))
+        else:
+            notificator.SendUnsuccessfullyMoneyTransfer(int(data.get("Payerid")), int(data.get("Amount")),
+                                                        int(data.get("recipient_id")))
     except Exception as e:
         logging.error(f"Error processing payment: {e}")
+        notificator.SendUnsuccessfullyMoneyTransfer(int(data.get("Payerid")), int(data.get("Amount")),
+                                                  int(data.get("recipient_id")))
