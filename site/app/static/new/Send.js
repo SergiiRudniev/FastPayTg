11c5f33159bcca6ee1890643df0b46fc5a30e827
Sergii Rudniev <100538048+SergiiRudniev@users.noreply.github.com>
2024-08-05 23:11:06 +0200

    Add files via upload

--- site/app/static/new/Send.js
+++ site/app/static/new/Send.js
@@ -1,14 +1,10 @@
-// Проверка наличия объекта Telegram.WebApp
 if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 'undefined') {
     let tg = window.Telegram.WebApp;
 
-    // Развернуть интерфейс Telegram WebApp
     tg.expand();
 
-    // URL для получения баланса пользователя
     const balanceUrl = `https://saved-surely-crane.ngrok-free.app/api/getbalance/${tg.initDataUnsafe.user.id}`;
 
-    // Функция для получения и отображения баланса пользователя
     function fetchBalance() {
         fetch(balanceUrl)
             .then(response => {
@@ -26,9 +22,7 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
             });
     }
 
-    // Функция для отправки денег, используя ID получателя и сумму из полей ввода
     function sendMoney() {
-        // Получение значений из полей ввода
         const recipient = document.getElementById('input-field').value.trim();
         const amount = document.getElementById('summ-field').value.trim();
 
@@ -43,7 +37,6 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
             return;
         }
 
-        // Конструкция URL для отправки денег
         const sendUrl = `https://saved-surely-crane.ngrok-free.app/api/send/${recipient}`;
 
         const sendData = {
@@ -51,7 +44,6 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
             Amount: Number(amount)
         };
 
-        // Отправка POST-запроса для инициации транзакции
         fetch(sendUrl, {
             method: 'POST',
             headers: {
@@ -68,7 +60,6 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
             return response.json();
         })
         .then(() => {
-            // Открыть модальное окно для ввода кода
             openCodeModal();
         })
         .catch(error => {
@@ -77,25 +68,20 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
         });
     }
 
-    // Функция для открытия модального окна
     function openCodeModal() {
         const modal = document.getElementById('codeModal');
         const closeButton = modal.querySelector('.close');
         const submitButton = document.getElementById('submitCode');
         const codeInput = document.getElementById('codeInput');
 
-        // Показать модальное окно
         modal.style.display = 'block';
 
-        // Очистка поля ввода кода
         codeInput.value = '';
 
-        // Обработчик закрытия модального окна
         closeButton.onclick = function() {
             modal.style.display = 'none';
         }
 
-        // Обработчик отправки кода
         submitButton.onclick = function() {
             const code = codeInput.value.trim();
             if (code === '') {
@@ -103,11 +89,9 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
                 return;
             }
 
-            // Проверка кода
             verifyCode(code);
         }
 
-        // Закрытие модального окна при клике вне его
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = 'none';
@@ -115,7 +99,6 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
         }
     }
 
-    // Функция для проверки кода
     function verifyCode(inputCode) {
         const sendCodeUrl = `https://saved-surely-crane.ngrok-free.app/api/check_code/${tg.initDataUnsafe.user.id}`;
         const codeData = {
@@ -133,15 +116,12 @@ if (typeof window.Telegram !== 'undefined' && typeof window.Telegram.WebApp !== 
         .then(responseData => {
             if (responseData["Status:"] === "ok") {
                 alert('Successfully!');
-                // Закрытие модального окна после успешной проверки
                 document.getElementById('codeModal').style.display = 'none';
             } else if (responseData["Status:"] === "warning") {
                 alert('The code has expired. Attempts are closed.');
-                // Закрытие модального окна при истекшем коде
                 document.getElementById('codeModal').style.display = 'none';
             } else if (responseData["Status:"] === "ErrorCode") {
                 alert('Incorrect code. Please try again.');
-                // Повторное открытие модального окна при неправильном коде
                 openCodeModal();
             } else {
                 throw new Error('Unknown error occurred.');
